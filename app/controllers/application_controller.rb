a524167cf0dac180676bbfeff7f33215c3fc775d
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-05 12:27:06 +0900

    user コントローラー設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(configure_permitted_parameters)
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+  end
 end
