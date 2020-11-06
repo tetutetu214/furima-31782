61469c5a7567ea822995bb1ed6f16c05c8c74a8f
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-06 18:22:04 +0900

    ActiveHash　実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,17 @@
 class Item < ApplicationRecord
 
  belongs_to :user
+ extend ActiveHash::Associations::ActiveRecordExtensions
+ belongs_to :category
+ belongs_to :item_status
+ belongs_to :fee_status
+ belongs_to :prefecture
+ belongs_to :scheduled_delivery
 
+ validates :category_id, numericality: { other_than: 1 } 
+ validates :item_status_id, numericality: { other_than: 1 } 
+ validates :fee_status_id, numericality: { other_than: 1 } 
+ validates :prefecture_id, numericality: { other_than: 1 } 
+ validates :scheduled_delivery_id, numericality: { other_than: 1 } 
+ 
 end
