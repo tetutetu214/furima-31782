d3d14518a11ec562b17cc68233d8a8f39fbf8287
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-06 20:55:25 +0900

    validates 実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,18 @@ class Item < ApplicationRecord
  belongs_to :scheduled_delivery
  has_one_attached :image
 
- 
- validates :category_id, numericality: { other_than: 1 } 
+ with_options presence: true do
+  validates :image
+  validates :name
+  validates :info
+  validates :price
+  end  
+
+  validates :price, format: {with: /\A[0-9]+\z/}
+  validates :price, numericality: {only_integer: true,
+  greater_than: 300, less_than: 9999999}
+
+  validates :category_id, numericality: { other_than: 1 } 
  validates :item_status_id, numericality: { other_than: 1 } 
  validates :fee_status_id, numericality: { other_than: 1 } 
  validates :prefecture_id, numericality: { other_than: 1 } 
