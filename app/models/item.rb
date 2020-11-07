9d586d9762c1e573c2b361e30b899a4cd118a686
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-07 17:10:55 +0900

    レビュー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,14 +14,16 @@ class Item < ApplicationRecord
     validates :info
     validates :price
   end
-
+  
+  with_options numericality:{ other_than: 1} do
+    validates :category_id
+    validates :item_status_id
+    validates :fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+  
   validates :price, format: { with: /\A[0-9]+\z/ }
   validates :price, numericality: { only_integer: true,
                                     greater_than: 299, less_than: 9_999_999 }
-
-  validates :category_id, numericality: { other_than: 1 }
-  validates :item_status_id, numericality: { other_than: 1 }
-  validates :fee_status_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :scheduled_delivery_id, numericality: { other_than: 1 }
 end
