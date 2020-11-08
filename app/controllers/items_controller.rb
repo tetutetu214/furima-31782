8b82b1ba06cdacbf4eb6fb3ec5a89c795ca941ed
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-08 15:28:41 +0900

    V 表示変更実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.save(item_params)
-      redirect_to root_path
+    if @item.update(item_params)
+      redirect_to item_path(@item)
     else
       render :edit
     end
