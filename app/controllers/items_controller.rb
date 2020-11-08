9d8a4e62eb5a17d4af99fdf26760e218a6276b25
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-08 14:31:33 +0900

    レビュー再々修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_tweet, only: [:show]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :info, :category_id, :item_status_id, :fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def set_tweet
+  def set_item
     @item = Item.find(params[:id])
   end
 
