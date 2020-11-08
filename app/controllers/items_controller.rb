7cd17cad21fb065f250b19f77ccaecb8825dfe58
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-08 14:57:21 +0900

    C 本人以外編集出来ない実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,9 +23,15 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 
   def update
+    if @item.save(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
