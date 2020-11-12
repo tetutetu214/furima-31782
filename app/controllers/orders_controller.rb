2ef980f4731b4a1d6729dd908959e8964591b1c7
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-12 11:28:21 +0900

    C ルート設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :redirect
+
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
@@ -24,5 +27,9 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id], token: params[:token])
+  end
+
+  def redirect
+    redirect_to root_path if current_user.id 
   end
 end
