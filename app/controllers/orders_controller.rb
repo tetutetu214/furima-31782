ff2078acafb28ba7dca2e29dccbb9e8dbda3fe6b
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-12 22:17:04 +0900

    微調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :redirect
+  before_action :redirect_to_me
+  before_action :sold_out
 
   def index
     @item = Item.find(params[:item_id])
@@ -31,9 +32,14 @@ class OrdersController < ApplicationController
     params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  def redirect
+  def redirect_to_me
     item = Item.find(params[:item_id])
-    id = item.orders
-    redirect_to root_path if id.present?
+    redirect_to root_path if current_user.id == item.user_id
+  end
+
+  def sold_out
+    item = Item.find(params[:item_id])
+    sold_out = item.orders
+    redirect_to root_path if sold_out.present?
   end
 end
