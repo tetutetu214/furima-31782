2c6ea571b49a9f6f923543b8bb72a7aa1b9e5772
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-12 13:02:31 +0900

    render 設定実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  
 
   def index
     @items = Item.all.order('created_at DESC')
+    
   end
 
   def new
@@ -23,7 +25,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id  
+      redirect_to action: :index 
+    else 
+      redirect_to root_path if @item.orders.present?
+    end
   end
 
   def update
@@ -50,4 +56,5 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
 end
