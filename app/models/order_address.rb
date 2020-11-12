5a8593a94469f473a458b233b0bf165175d17510
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-12 10:59:25 +0900

    バリデーション　設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,16 +6,18 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :token
-
     validates :postal_code
     validates :prefecture_id ,numericality: { other_than: 1 }
     validates :city
     validates :address
     validates :phone_number
   end
-
-  def save
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/	}
+    validates :phone_number, format: { with: /\A\d{11}\z/	}
+    
+    def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
+  
 end
