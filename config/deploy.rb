aa02487fc0aeeaa2ee85c1b983648ccdc5b53dff
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-15 11:53:13 +0900

    EC2 自動デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url,  'git@github.com:tetutetu214/furima-31782.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, ’2.6.5’
+set :rbenv_ruby, '2.6.5'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
